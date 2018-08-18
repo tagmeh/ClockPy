b9c48a14a26c5a19ce2b0e6400077b3a388e5f28
Justin <yourpocketlint@gmail.com>
2018-08-18 17:07:29 +0000

    Now with Snooze function

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ class DigitalClock(FloatLayout):
     alarm_switch = NumericProperty(0)       # Alarm On/Off Switch
     alarm_active = NumericProperty(0)       # Initial Alarm Call
     is_alarming = NumericProperty(0)        # Alarm Loop
-    nfc_read = list(('04', '33', '3A', '4A', '22', '4B', '81'))
+    nfc_read = list(('04', '33', '3A', '4A', '22', '4B', '81')) #Starting list structure for NFC tags
     rando = NumericProperty(0)
     section = NumericProperty(1)            # Section of audio files to use 1=1-7, 2=8-14, 3=15-21
     play_num = NumericProperty(0)
@@ -31,6 +31,7 @@ class DigitalClock(FloatLayout):
     audio_file = StringProperty("")
     colour = NumericProperty(0)
     alarm_event = ()
+    snooze = ()
 
     def update(self, dt=0):
         self.display_time = time.strftime("%H : %M")
@@ -49,10 +50,16 @@ class DigitalClock(FloatLayout):
         if (self.display_time == self.alarm_time) and (self.alarm_switch == 1) and (self.is_alarming == 0):
             self.is_alarming = 1
             print("!!ALARM!!")
-            Clock.schedule_once(self.update, secs_to_next_minute)
-            self.alarm_event = Clock.schedule_interval(self.alarm_loop, 1)
+            Clock.schedule_once(self.update, secs_to_next_minute + 1) #add 1 second to ensure it updates after the new minute
+            events = Clock.get_events()
+            print(events)
+            self.schedule_alarm()
         else:
             Clock.schedule_once(self.update, secs_to_next_minute)
+
+    #Making schedule_alarm its own function to also be called when I put in the snooze function
+    def schedule_alarm(self, dt=0):
+        self.alarm_event = Clock.schedule_interval(self.alarm_loop, 1)
 
 
     def alarm_loop(self, *args):
@@ -95,6 +102,16 @@ class DigitalClock(FloatLayout):
 
 
     # TODO - Add Snooze Function
+    def snooze_func(self, *args):
+        if (self.is_alarming == 1):
+            Clock.unschedule(self.alarm_event)
+            Clock.unschedule(self.snooze)
+            self.snooze = Clock.schedule_once(self.schedule_alarm, 240)
+            self.colour = 0
+            events = Clock.get_events()
+            print(events)
+            #if music.get_busy() == True:
+                #music.stop()
 
     def click_settings(self, *args):
         if args[1] == 'down':
@@ -118,6 +135,9 @@ class DigitalClock(FloatLayout):
         else:
             self.alarm_switch = 0
             self.colour = 0
+            self.is_alarming = 0
+            Clock.unschedule(self.schedule_alarm)
+            Clock.unschedule(self.snooze)
             print("Alarm Off: " + str(self.alarm_switch))
 
     def hour10_up(self):
@@ -197,7 +217,7 @@ class DigitalClockApp(App):
 
 
 if __name__ == '__main__':
-    #Config.set('graphics', 'fullscreen', 'auto')              ####ENABLE FULLSCREEN####
-    #Config.set('graphics', 'window_state', 'maximized')        ###MAY NOT NEED THIS ONE###
-    #Config.write()
+    Config.set('graphics', 'fullscreen', '0')              ####ENABLE FULLSCREEN#### 'auto'=fullscreen / '0'=not-fullscreen
+    Config.set('graphics', 'window_state', 'normal')        ###MAY NOT NEED THIS ONE###
+    Config.write()
     DigitalClockApp().run()
