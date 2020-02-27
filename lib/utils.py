2d7581502cdab1e221161cedac50e6864c5dc356
John <tagmeh@gmail.com>
2020-02-26 19:33:37 -0600

    Refactored the config file creation function to use a local config

--- lib/utils.py
+++ lib/utils.py
@@ -12,7 +12,10 @@ def create_default_config_file():
     Copies the contents of "default_config.txt" over to "config.ini".
     Creates the config.ini file if it doesn't already exist.
     """
-    with open(os.path.join(constants.FILES, 'default_config.txt'), 'r') as f:  # open for reading
-        with open(os.path.join(constants.FILES, 'config.ini'), 'w+') as c:  # open/create for writing
-            for line in f.readlines():
-                c.write(line)
+
+    with open(os.path.join(constants.FILES, 'config.ini'), 'w+') as c:  # open/create for writing
+        # for line in constants.DEFAULT_CONFIG.splitlines():
+        c.write(constants.DEFAULT_CONFIG)
+
+
+
